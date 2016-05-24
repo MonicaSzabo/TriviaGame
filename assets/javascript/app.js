a7186ec3d0dc26cfc5ae5f54f27907c52453df34
Monica Szabo <monicaszb@gmail.com>
2016-05-23 21:58:23 -0500

    added pics and questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+	var qaArray;
+
+	function varSet() {
+		qaArray = [{
+			question: "What was Joss Whedon's first writing job?",
+			answers: ["Growing Pains", "Clarissa Explains It All", "Roseanne", "The Wonder Years"],
+			pic: 'assets/images/roseanne.gif',
+			correctanswer: 2
+		}, {
+			question: "Which is NOT a potential monster on the <em>Cabin in the Woods</em> whiteboard?",
+			answers: ["Kevin", "Sugarplum Fairy", "Vampires", "Giant Spiders"],
+			pic: 'assets/images/cabininthewoods.gif',
+			correctanswer: 3
+		}, {
+			question: "What is the standard length of a Doll's contract in <em>Dollhouse</em>?",
+			answers: ["10 years", "5 years", "2 years", "1 year"],
+			pic: 'assets/images/dollhouse.gif',
+			correctanswer: 1
+		}, {
+			question: "Which of Joss Whedon’s works garnered him his first-ever Emmy win?",
+			answers: ["Buffy the Vampire Slayer", "Firefly", "Dollhouse", "Dr. Horrible’s Sing-Along Blog"],
+			pic: 'assets/images/drhorrible.gif',
+			correctanswer: 3
+		}, {
+			question: "Which Pixar movie did Joss Whedon completely rewrite?",
+			answers: ["Toy Story", "Finding Nemo", "Monsters, Inc", "A Bug’s Life"],
+			pic: 'assets/images/toystory.gif',
+			correctanswer: 0
+		}, {
+			question: "At the series conclusion, how old is Angel in <em>Angel</em>?",
+			answers: ["32 years old", "188 years old", "251 years old", "363 years old"],
+			pic: 'assets/images/angel.gif',
+			correctanswer: 2
+		}, {
+			question: "Which founding Avenger member was not in <em>The Avengers</em> (2012)?",
+			answers: ["Thor", "Ant-Man", "Hawkeye", "Black Widow"],
+			pic: 'assets/images/avengers.gif',
+			correctanswer: 1
+		}, {
+			question: "What is the <q>documented</q> reason Fox cancelled <em>Firefly</em>?",
+			answers: ["Mal shot too many people", "It was too dark", "It’s ratings", "Fox executives don’t know good shows when they see them"],
+			pic: 'assets/images/firefly.gif',
+			correctanswer: 1
+		}, {
+			question: "Which Shakespeare play did Joss Whedon direct and produce in 2012?",
+			answers: ["Much Ado About Nothing", "A Midsummer Night’s Dream", "Twelfth Night", "The Taming of the Shrew"],
+			pic: 'assets/images/muchadoaboutnothing.gif',
+			correctanswer: 0
+		}, {
+			question: "How many times did Buffy die in <em>Buffy the Vampire Slayer</em>?",
+			answers: ["Never", "Once", "Twice", "Three Times"],
+			pic: 'assets/images/buffy.gif',
+			correctanswer: 2
+		}]
+
+	}
+
+
+
+
+
+	varSet();
+	console.log(qaArray[0].question)
+	
+});
